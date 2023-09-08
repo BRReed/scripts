0a2dddb2bce7a4ad42d836c0e6fa74b2d11df2c5
BRReed <b.r.reed@gmail.com>
2023-09-07 22:00:04 -0700

    added virt-manager conditional
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- arch-based/get-virt.sh
+++ arch-based/get-virt.sh
@@ -2,8 +2,16 @@
 
 if ( LC_ALL=C lscpu | grep Virtualization )
 then
-  echo "->Virtualization parameters are enabled."
+  echo "-> Virtualization parameters are enabled."
 else
-  echo "->Please enable Virtualization paramaters and run this script again."
+  echo "-> Please enable Virtualization paramaters and run this script again."
   exit
+fi
+
+if ( which virt-manager )
+then
+  echo "-> virt-manager is installed"
+else
+  echo "-> installing virt-manager"
+  yes | pacman -S virt-manager
 fi
