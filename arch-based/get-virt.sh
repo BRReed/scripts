73b83e494768854639ced5fb2d516b06e7eeb2f8
BRReed <b.r.reed@gmail.com>
2023-09-07 21:54:14 -0700

    Virtualization enabled conditional added to get-virt.sh
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- arch-based/get-virt.sh
+++ arch-based/get-virt.sh
@@ -1,1 +1,9 @@
-#!/bin/bash
+#!/bin/sh
+
+if ( LC_ALL=C lscpu | grep Virtualization )
+then
+  echo "->Virtualization parameters are enabled."
+else
+  echo "->Please enable Virtualization paramaters and run this script again."
+  exit
+fi
