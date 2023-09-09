c2200c968d5c99d1115001d1bb3d25d20a23f11b
BRReed <b.r.reed@gmail.com>
2023-09-08 21:11:10 -0700

    remove virtualization enabled conditional from script
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- arch-based/get-virt.sh
+++ arch-based/get-virt.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-if ( LC_ALL=C lscpu | grep Virtualization )
-then
-  echo "-> Virtualization parameters are enabled."
-else
-  echo "-> Please enable Virtualization paramaters and run this script again."
-  exit
-fi
-
 if ( which virt-manager )
 then
   echo "-> virt-manager is installed"
