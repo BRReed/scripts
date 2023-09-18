6ed24eab2080382e3acd3e485db63554996ed30e
BRReed <b.r.reed@gmail.com>
2023-09-17 22:22:29 -0700

    reduced conditionals for debian based, as all you need to install is virt-manager and apt takes care of all dependencies
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- debian-based/get-virt.sh
+++ debian-based/get-virt.sh
@@ -7,19 +7,3 @@ else
   echo "-> installing virt-manager"
   apt install virt-manager -y
 fi
-
-if ( which kvm )
-then
-  echo "-> qemu-kvm is installed"
-else
-  echo "-> installing qemu-kvm"
-  apt install qemu-kvm -y
-fi
-
-if ( which libvirtd )
-then
-  echo "-> libvirt is installed"
-else
-  echo "-> installing libvirt"
-  apt install libvirt -y
-fi
