fb54f02fa7521604b147a0f8d336ad7d68377bb5
BRReed <b.r.reed@gmail.com>
2023-10-16 16:00:44 -0700

    check if docker.list exists and is populated with distro
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- ubuntu/get-docker.sh
+++ ubuntu/get-docker.sh
@@ -40,3 +40,15 @@ else
   echo "-> getting docker.gpg"
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 fi 
+
+if ( cat /etc/apt/sources.list.d/docker.list | grep ubuntu )
+then
+  echo "-> docker.list already populated"
+else
+  echo "-> Populating docker.list"
+  echo \
+    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+fi
