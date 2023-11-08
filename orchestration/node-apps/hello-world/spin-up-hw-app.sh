68afb9b60c2c141965ac21c402a5db69ad2c6e7a
BRReed <b.r.reed@gmail.com>
2023-11-07 19:19:31 -0800

    virsh, etc checks
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- orchestration/node-apps/hello-world/spin-up-hw-app.sh
+++ orchestration/node-apps/hello-world/spin-up-hw-app.sh
@@ -1,18 +1,42 @@
 #!/bin/sh
 
 
-if [ -d "./ubuntu/" ]
-then
-  echo "ubuntu sub directory exists"
-else
-  echo "making directory ./ubuntu"
-  mkdir ./ubuntu/
-fi
-
-if [ -f "./ubuntu/spin-up-jammy.sh" ] 
+if [ -f "./spin-up-jammy.sh" ] 
 then
   echo "Ubuntu Jammy spin up script exists"
 else
   echo "Getting spin-up-jammy.sh"
-  curl -o ./ubuntu/spin-up-jammy.sh https://raw.githubusercontent.com/BRReed/scripts/main/vm-management/cloud-init-configs/ubuntu/spin-up-jammy.sh
+  curl -o ./spin-up-jammy.sh https://raw.githubusercontent.com/BRReed/scripts/main/vm-management/cloud-init-configs/ubuntu/spin-up-jammy.sh
 fi
+
+if [ -f ./get-virt.sh ]
+then
+  echo "get-virt.sh exists"
+else
+  echo "getting get-virt.sh"
+  curl -o get-virt.sh https://raw.githubusercontent.com/BRReed/scripts/main/get-virt-manager/debian-based/get-virt.sh
+fi
+
+sh get-virt.sh
+
+if ( virsh list | grep jammy-cloud )
+then
+  echo "jammy-cloud vm running"
+else
+  echo "spinning up jammy-cloud vm"
+  sh ./ubuntu/spin-up-jammy.sh &
+fi
+
+i=0
+# wait for jammy-cloud to finish initialization to continue script 
+while [ $i -ne 1 ]
+do 
+  if ( virsh list | grep jammy-cloud )
+  then
+    i=1
+  else
+    echo "sleeping"
+    sleep 60
+  fi
+done
+
