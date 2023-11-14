43e5a0863f0d332f94a0f4d81a99ce844cf1ada2
BRReed <b.r.reed@gmail.com>
2023-11-13 20:35:18 -0800

    added get node, dockerfile conditionals
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- orchestration/node-apps/hello-world/spin-up-hw-app.sh
+++ orchestration/node-apps/hello-world/spin-up-hw-app.sh
@@ -46,15 +46,16 @@ do
   fi
 done
 
+# GET DOCKER COMMANDS
+
 if [ -f "./get-docker.sh" ]
 then
-  echo "get docker script exists"
+  echo "get docker script exists locally"
 else
   echo "getting get-docker.sh"
   curl -o ./get-docker.sh https://raw.githubusercontent.com/BRReed/scripts/main/get-docker/ubuntu/get-docker.sh
 fi
 
-# check if get-docker script already exists on vm
 if ( ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') [ -f ./get-docker.sh ] )
 then
   echo "get-docker.sh exists on jammy-cloud"
@@ -69,4 +70,48 @@ then
 else
   echo "getting docker on remote vm"
   ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') 'bash < ./get-docker.sh'
+fi
+
+# GET NODE COMMANDS
+
+if [ -f "./get-node.sh" ]
+then
+  echo "get node script exists locally"
+else
+  echo "getting get node script"
+  curl -o ./get-node.sh https://raw.githubusercontent.com/BRReed/scripts/main/get-node/ubuntu/get-node.sh
+fi
+
+if ( ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') [ -f ./get-node.sh ] )
+then
+  echo "get-node.sh exists on jammy-cloud"
+else
+  echo "getting get-docker.sh onto jammy-cloud"
+  cat ./get-node.sh | ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') "cat > ./get-node.sh"
+fi
+
+if ( ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') 'dpkg -s npm' )
+then
+  echo "npm exists on remote vm"
+else
+  echo "getting npm on remote vm"
+  ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') 'bash < ./get-node.sh'
+fi
+
+# DOCKERFILE COMMANDS
+
+if [-f "./Dockerfile" ]
+then
+  echo "dockerfile exists locally"
+else
+  echo "getting dockerfile"
+  curl -o ./Dockerfile https://raw.githubusercontent.com/BRReed/hello-relativepath/main/scripts/docker/hello-world-app/Dockerfile
+fi
+
+if ( ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') [ -f ./Dockerfile ] )
+then
+  echo "dockerfile exists on jammy-cloud"
+else
+  echo "getting dockerfile onto jammy-cloud"
+  cat ./Dockerfile | ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') "cat > ./Dockerfile"
 fi
