a089677677e63a0f6a78443293ee0feb9b121d18
BRReed <b.r.reed@gmail.com>
2023-11-15 17:35:11 -0800

    removed download of spin up jammy script, changed to curlbash command
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- orchestration/node-apps/hello-world/spin-up-hw-app.sh
+++ orchestration/node-apps/hello-world/spin-up-hw-app.sh
@@ -16,20 +16,12 @@ else
   sudo apt install virtinst -y
 fi
 
-if [ -f "./spin-up-jammy.sh" ] 
-then
-  echo "Ubuntu Jammy spin up script exists"
-else
-  echo "Getting spin-up-jammy.sh"
-  curl -o ./spin-up-jammy.sh https://raw.githubusercontent.com/BRReed/scripts/main/vm-management/cloud-init-configs/ubuntu/spin-up-jammy.sh
-fi
-
 if ( virsh list | grep jammy-cloud )
 then
   echo "jammy-cloud vm running"
 else
   echo "spinning up jammy-cloud vm"
-  sh ./spin-up-jammy.sh&
+  curl https://raw.githubusercontent.com/BRReed/scripts/main/vm-management/ubuntu/spin-up-jammy.sh | bash
 fi
 
 i=0
