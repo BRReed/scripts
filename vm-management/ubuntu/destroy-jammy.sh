3330615894c37e5d6186b8e34c1a41ddfa8f0c8a
BRReed <b.r.reed@gmail.com>
2023-11-17 15:33:46 -0800

    added rm user-data to single line
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- vm-management/ubuntu/destroy-jammy.sh
+++ vm-management/ubuntu/destroy-jammy.sh
@@ -2,5 +2,4 @@
 
 
 ssh-keygen -f ~/.ssh/known_hosts -R "$(virsh domifaddr ${VM} | awk -F'[ /]+' '{if (NR>2) print $5}')"
-virsh destroy $VM && virsh undefine $VM --remove-all-storage
-rm user-data
+virsh destroy $VM && virsh undefine $VM --remove-all-storage && rm user-data
