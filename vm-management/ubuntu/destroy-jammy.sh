d76ab2638d80afb62a7b1e47b2e4d9ec9c5761e8
BRReed <b.r.reed@gmail.com>
2023-11-17 14:12:12 -0800

    removed conditionals and just delete files
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- vm-management/ubuntu/destroy-jammy.sh
+++ vm-management/ubuntu/destroy-jammy.sh
@@ -2,17 +2,5 @@
 
 
 ssh-keygen -f ~/.ssh/known_hosts -R "$(virsh domifaddr ${VM} | awk -F'[ /]+' '{if (NR>2) print $5}')"
-
-if !(virsh list --all | grep " ${VM} ") then
-  echo "jammy-cloud vm does not exist"
-else
-  echo "destroy, undefine and removing all storage for jammy-cloud vm"
-  virsh destroy $VM && virsh undefine $VM --remove-all-storage
-fi
-
-if !(find user-data) then
-  echo "user-data does not exist"
-else
-  echo "removing user-data"
-  rm user-data
-fi
+virsh destroy $VM && virsh undefine $VM --remove-all-storage
+rm user-data
