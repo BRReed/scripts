407ea30fcf0780c45b5c71ee2fb5afd5a7b0c2ed
BRReed <b.r.reed@gmail.com>
2023-11-17 14:00:45 -0800

    moved dockerfile, package.json, hello-world-app.js conditionals to node-deploy.sh from spin-up-hw-app.sh
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- orchestration/node-apps/hello-world/spin-up-hw-app.sh
+++ orchestration/node-apps/hello-world/spin-up-hw-app.sh
@@ -41,29 +41,7 @@ else
   ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') 'curl https://raw.githubusercontent.com/BRReed/scripts/main/get-node/ubuntu/get-node.sh | bash'
 fi
 
-if ( ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') "stat ./Dockerfile" )
-then
-  echo "dockerfile exists on jammy-cloud"
-else
-  echo "getting dockerfile onto jammy-cloud"
-  ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') "curl -o ./Dockerfile https://raw.githubusercontent.com/BRReed/hello-relativepath/main/scripts/docker/hello-world-app/Dockerfile"
-fi
-
-if ( ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') "stat ./package.json" )
-then
-  echo "package.json exists on jammy-cloud"
-else
-  echo "getting package.json onto jammy-cloud"
-  ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') "curl -o ./package.json https://raw.githubusercontent.com/BRReed/hello-relativepath/main/scripts/docker/hello-world-app/package.json"
-fi
-
-if ( ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') [ -f ./hello-world-app.js ] )
-then
-  echo "hello-world-app exists on jammy-cloud"
-else
-  echo "getting hello-world-app onto jammy-cloud"
-  ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') "curl -o ./hello-world-app.js https://raw.githubusercontent.com/BRReed/hello-relativepath/main/hello-world-app.js"
-fi
+./node-deploy.sh
 
 if ( ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') "curl localhost:3000" )
 then
