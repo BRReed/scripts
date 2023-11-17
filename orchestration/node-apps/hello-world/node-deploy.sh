9a9366a5d77709af6622d0ba7469bfc7e7489e56
BRReed <b.r.reed@gmail.com>
2023-11-17 14:56:11 -0800

    changed file to work on machine its running on
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- orchestration/node-apps/hello-world/node-deploy.sh
+++ orchestration/node-apps/hello-world/node-deploy.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-if ( ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') "stat ./Dockerfile" )
+if (stat ./Dockerfile)
 then
   echo "dockerfile exists on jammy-cloud"
 else
   echo "getting dockerfile onto jammy-cloud"
-  ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') "curl -o ./Dockerfile https://raw.githubusercontent.com/BRReed/hello-relativepath/main/scripts/docker/hello-world-app/Dockerfile"
+  curl -o ./Dockerfile https://raw.githubusercontent.com/BRReed/hello-relativepath/main/scripts/docker/hello-world-app/Dockerfile
 fi
 
-if ( ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') "stat ./package.json" )
+if (stat ./package.json)
 then
   echo "package.json exists on jammy-cloud"
 else
   echo "getting package.json onto jammy-cloud"
-  ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') "curl -o ./package.json https://raw.githubusercontent.com/BRReed/hello-relativepath/main/scripts/docker/hello-world-app/package.json"
+  curl -o ./package.json https://raw.githubusercontent.com/BRReed/hello-relativepath/main/scripts/docker/hello-world-app/package.json
 fi
 
-if ( ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') "stat ./hello-world-app.js" )
+if (stat ./hello-world-app.js)
 then
   echo "hello-world-app exists on jammy-cloud"
 else
   echo "getting hello-world-app onto jammy-cloud"
-  ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') "curl -o ./hello-world-app.js https://raw.githubusercontent.com/BRReed/hello-relativepath/main/hello-world-app.js"
+  curl -o ./hello-world-app.js https://raw.githubusercontent.com/BRReed/hello-relativepath/main/hello-world-app.js
 fi
