bc64ce5090f08a1647c0c54215d3d10460930656
BRReed <b.r.reed@gmail.com>
2023-11-17 21:39:57 -0800

    added systemctl calls to node-deploy
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- orchestration/node-apps/hello-world/node-deploy.sh
+++ orchestration/node-apps/hello-world/node-deploy.sh
@@ -23,3 +23,29 @@ else
   echo "getting hello-world-app onto jammy-cloud"
   curl -o ./hello-world-app.js https://raw.githubusercontent.com/BRReed/hello-relativepath/main/hello-world-app.js
 fi
+
+if ( npm ls )
+then
+  echo "Dependencies have been installed"
+else
+  echo "Installing dependencies"
+  npm install
+fi
+
+if (stat /etc/systemd/system/hello-relpath.service)
+then
+  echo "hello relpath service file exists"
+else
+  echo "getting hello-relpath service"
+  sudo curl -o /etc/systemd/system/hello-relpath.service https://raw.githubusercontent.com/BRReed/scripts/main//orchestration/node-apps/hello-world/hello-relpath.service
+fi
+
+if (systemctl is-active hello-relpath)
+then
+  echo "hello-relpath is running"
+else
+  echo "starting and enabling reboot on restart for hello-relpath"
+  sudo systemctl daemon-reload
+  sudo systemctl start hello-relpath.service
+  sudo systemctl enable hello-relpath.service
+fi
