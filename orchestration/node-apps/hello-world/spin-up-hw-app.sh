edc4b734765b7165652b08df1a5648365c1d8b46
BRReed <b.r.reed@gmail.com>
2023-11-18 11:52:14 -0800

    changed instances of brian to breed in ssh calls
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- orchestration/node-apps/hello-world/spin-up-hw-app.sh
+++ orchestration/node-apps/hello-world/spin-up-hw-app.sh
@@ -22,21 +22,21 @@ then
   echo "jammy-cloud vm running"
 else
   echo "spinning up jammy-cloud vm"
-  curl https://raw.githubusercontent.com/BRReed/scripts/main/vm-management/ubuntu/spin-up-jammy.sh | bash && while ( ! ssh -t -o StrictHostKeyChecking=no brian@$(virsh domifaddr $VM | awk -F'[ /]+' '{if (NR>2) print $5}') 2>/dev/null ); do echo -n "."; done
+  curl https://raw.githubusercontent.com/BRReed/scripts/main/vm-management/ubuntu/spin-up-jammy.sh | bash && while ( ! ssh -t -o StrictHostKeyChecking=no "${USER}"@$(virsh domifaddr $VM | awk -F'[ /]+' '{if (NR>2) print $5}') 2>/dev/null ); do echo -n "."; done
 fi
 
-if ( ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') 'dpkg -s npm' )
+if ( ssh -q "${USER}"@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') 'dpkg -s npm' )
 then
   echo "npm exists on remote vm"
 else
   echo "getting npm on remote vm"
-  ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') 'curl https://raw.githubusercontent.com/BRReed/scripts/main/get-node/ubuntu/get-node.sh | bash'
+  ssh -q "${USER}"@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') 'curl https://raw.githubusercontent.com/BRReed/scripts/main/get-node/ubuntu/get-node.sh | bash'
 fi
 
-if ( ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') "curl localhost:3000" )
+if ( ssh -q "${USER}"@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') "curl localhost:3000" )
 then
   echo "hello world app is started"
 else
   echo "starting hello-world-app"
-  ssh -q brian@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') "curl https://raw.githubusercontent.com/BRReed/scripts/breed/orches-relpath/orchestration/node-apps/hello-world/node-deploy.sh | bash"
+  ssh -q "${USER}"@$(virsh domifaddr jammy-cloud | awk -F'[ /]+' '{if (NR>2) print $5}') "curl https://raw.githubusercontent.com/BRReed/scripts/breed/orches-relpath/orchestration/node-apps/hello-world/node-deploy.sh | bash"
 fi
