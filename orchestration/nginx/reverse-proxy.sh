7752c7b41ee86dfdc3d70115c60b3326eaf40fc7
BRReed <b.r.reed@gmail.com>
2023-12-03 20:19:40 -0800

    added conditional for hw-app.config file
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- orchestration/nginx/reverse-proxy.sh
+++ orchestration/nginx/reverse-proxy.sh
@@ -8,3 +8,11 @@ else
   echo "installing nginx"
   sudo apt install nginx -y
 fi
+
+if (sudo stat /etc/nginx/sites-available/hw-app.config)
+then
+  echo "hw-app config file exists"
+else
+  echo "getting hw-app.config"
+  sudo curl -o /etc/nginx/sites-available/hw-app.config PLACEHOLDER FOR CONFIG URL
+fi
