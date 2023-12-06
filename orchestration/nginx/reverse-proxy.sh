7f1abc8c926c0ec6f393eaf85fdaf5a9fb640ef7
BRReed <b.r.reed@gmail.com>
2023-12-05 20:20:33 -0800

    added enabled hw-app check
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- orchestration/nginx/reverse-proxy.sh
+++ orchestration/nginx/reverse-proxy.sh
@@ -16,3 +16,11 @@ else
   echo "getting hw-app.config"
   sudo curl -o /etc/nginx/sites-available/hw-app.config PLACEHOLDER FOR CONFIG URL
 fi
+
+if (sudo stat /etc/nginx/sites-enabled/hw-app.config)
+then
+  echo "hw-app is enabled"
+else
+  echo "enabling hw-app"
+  sudo ln -s /etc/nginx/sites-available/hw-app.config /etc/nginx/sites-enabled/
+fi
